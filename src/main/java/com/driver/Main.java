ffb6d9e15c32c7398d30c1c194034d94986b95f3
Ahmed Lafeef EN <lafeefen@gmail.com>
2022-10-31 17:07:23 +0530

    method overridden

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,15 +4,15 @@ class A{
         return "Invoking method from class A";
     }
 }
-class B extends A{
+class B extends A {
     public String meth(){
         return "Method is overridden in Extendend class B";
     }
 }
 public class Main {
-    public static void main(String[] args) throws Exception {
-        B objB = new B();
-        String result = objB.meth();
-        System.out.println(result);
+    public static void main(String[] args) {
+        B obj = new B();
+        String ans = obj.meth();
+        System.out.println(ans);
     }
 }
